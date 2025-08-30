b924d86be8a5b99b963b0c50ff3f66296d71f0ef
Zach <100164907@ccisd.net>
2025-08-30 16:57:29 -0500

    Update NeonScript Interpreter.c

--- NeonScript Interpreter.c
+++ NeonScript Interpreter.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 #define MAX_VARS 100
 
@@ -42,27 +45,44 @@ void set_var(const char* name, const char* value) {
 // Helper: Set terminal text color using ANSI escape codes
 void set_text_color(const char* color) {
     if (strcmp(color, "red") == 0) {
-        printf("\033[0;31m");
+        printf("\033[31m");
+        fflush(stdout);
     } else if (strcmp(color, "green") == 0) {
-        printf("\033[0;32m");
+        printf("\033[32m");
+        fflush(stdout);
     } else if (strcmp(color, "yellow") == 0) {
-        printf("\033[0;33m");
+        printf("\033[33m");
+        fflush(stdout);
     } else if (strcmp(color, "blue") == 0) {
-        printf("\033[0;34m");
+        printf("\033[34m");
+        fflush(stdout);
     } else if (strcmp(color, "magenta") == 0) {
-        printf("\033[0;35m");
+        printf("\033[35m");
+        fflush(stdout);
     } else if (strcmp(color, "cyan") == 0) {
-        printf("\033[0;36m");
+        printf("\033[36m");
+        fflush(stdout);
     } else if (strcmp(color, "white") == 0) {
-        printf("\033[0;37m");
+        printf("\033[37m");
+        fflush(stdout);
     } else if (strcmp(color, "reset") == 0) {
         printf("\033[0m");
+        fflush(stdout);
     } else {
         printf("Unknown color: %s\n", color);
     }
 }
 
 int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    // Enable ANSI escape sequences in Windows Console
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+#endif
+    
     if (argc != 2) {
         printf("Usage: %s <source-file.neo>\n", argv[0]);
         return 1;
@@ -95,6 +115,134 @@ int main(int argc, char *argv[]) {
                     end--;
                 }
                 set_text_color(start);
+            }
+        }
+        // REPEAT command
+        else if (strncmp(line, "repeat (", 8) == 0) {
+            int repeat_count = 0;
+            if (sscanf(line, "repeat (%d)", &repeat_count) == 1 && repeat_count > 0) {
+                char repeat_line[256];
+                // Read the next line (the command to repeat)
+                if (fgets(repeat_line, sizeof(repeat_line), fp)) {
+                    size_t repeat_len = strlen(repeat_line);
+                    if (repeat_len > 0 && repeat_line[repeat_len - 1] == '\n') 
+                        repeat_line[repeat_len - 1] = '\0';
+                    
+                    // Remove leading whitespace/indentation
+                    char* cmd = repeat_line;
+                    while (*cmd == ' ' || *cmd == '\t') cmd++;
+                    
+                    // Execute the command repeat_count times
+                    for (int r = 0; r < repeat_count; r++) {
+                        // Handle PRINT command with variable substitution
+                        if (strncmp(cmd, "print ", 6) == 0) {
+                            char* msg = cmd + 6;
+                            char output[512] = "";
+                            for (int i = 0; msg[i];) {
+                                if (msg[i] == '$') {
+                                    // Find variable name
+                                    int j = i + 1;
+                                    char varname[64] = "";
+                                    int v = 0;
+                                    while (msg[j] && ((msg[j] >= 'a' && msg[j] <= 'z') || (msg[j] >= 'A' && msg[j] <= 'Z') || (msg[j] >= '0' && msg[j] <= '9') || msg[j] == '_')) {
+                                        varname[v++] = msg[j++];
+                                    }
+                                    varname[v] = '\0';
+                                    char* value = get_var(varname);
+                                    if (value)
+                                        strcat(output, value);
+                                    i = j;
+                                } else {
+                                    int l = strlen(output);
+                                    output[l] = msg[i];
+                                    output[l+1] = '\0';
+                                    i++;
+                                }
+                            }
+                            printf("%s\n", output);
+                        }
+                        // Handle other commands that might be repeated
+                        else if (strncmp(cmd, "neon.textcolor(", 16) == 0) {
+                            char color[64];
+                            if (sscanf(cmd, "neon.textcolor(%63[^)])", color) == 1) {
+                                char *start = color;
+                                while (*start == ' ') start++;
+                                char *end = start + strlen(start) - 1;
+                                while (end > start && (*end == ' ' || *end == '\n')) {
+                                    *end = '\0';
+                                    end--;
+                                }
+                                set_text_color(start);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // FOREVER command
+        else if (strncmp(line, "forever", 7) == 0) {
+            char forever_line[256];
+            // Read the next line (the command to repeat forever)
+            if (fgets(forever_line, sizeof(forever_line), fp)) {
+                size_t forever_len = strlen(forever_line);
+                if (forever_len > 0 && forever_line[forever_len - 1] == '\n') 
+                    forever_line[forever_len - 1] = '\0';
+                
+                // Remove leading whitespace/indentation
+                char* cmd = forever_line;
+                while (*cmd == ' ' || *cmd == '\t') cmd++;
+                
+                // Execute the command forever (infinite loop)
+                while (1) {
+                    // Handle PRINT command with variable substitution
+                    if (strncmp(cmd, "print ", 6) == 0) {
+                        char* msg = cmd + 6;
+                        char output[512] = "";
+                        for (int i = 0; msg[i];) {
+                            if (msg[i] == '$') {
+                                // Find variable name
+                                int j = i + 1;
+                                char varname[64] = "";
+                                int v = 0;
+                                while (msg[j] && ((msg[j] >= 'a' && msg[j] <= 'z') || (msg[j] >= 'A' && msg[j] <= 'Z') || (msg[j] >= '0' && msg[j] <= '9') || msg[j] == '_')) {
+                                    varname[v++] = msg[j++];
+                                }
+                                varname[v] = '\0';
+                                char* value = get_var(varname);
+                                if (value)
+                                    strcat(output, value);
+                                i = j;
+                            } else {
+                                int l = strlen(output);
+                                output[l] = msg[i];
+                                output[l+1] = '\0';
+                                i++;
+                            }
+                        }
+                        printf("%s\n", output);
+                    }
+                    // Handle other commands that might be repeated
+                    else if (strncmp(cmd, "neon.textcolor(", 16) == 0) {
+                        char color[64];
+                        if (sscanf(cmd, "neon.textcolor(%63[^)])", color) == 1) {
+                            char *start = color;
+                            while (*start == ' ') start++;
+                            char *end = start + strlen(start) - 1;
+                            while (end > start && (*end == ' ' || *end == '\n')) {
+                                *end = '\0';
+                                end--;
+                            }
+                            set_text_color(start);
+                        }
+                    }
+                    // Handle SET command
+                    else if (strncmp(cmd, "set ", 4) == 0) {
+                        char varname[64], varval[256];
+                        if (sscanf(cmd + 4, "%63s %255[^\n]", varname, varval) == 2) {
+                            set_var(varname, varval);
+                        }
+                    }
+                }
             }
         }
         // SET command
